d9f6709764cd839b2daa6012d643b0a22bea29e1
Anisa <111161857+anisar17@users.noreply.github.com>
2023-11-20 14:03:32 -0800

    updated microservice

--- microservice.js
+++ microservice.js
@@ -15,6 +15,14 @@ fs.readFile('fruits.txt', 'utf-8', (err, content) => {
     .then(data => {
       const nutrition = data.nutritions;
       console.log(nutrition);
+
+      fs.writeFile('fruits.txt', JSON.stringify(nutrition), 'utf-8', (err) => {
+        if (err) {
+          console.error('Failed to write to fruits.txt', err);
+        } else {
+          console.log('Data written to fruits.txt');
+        }
+      });
     })
     .catch(error => {
       console.error('Error fetching API.', error);
